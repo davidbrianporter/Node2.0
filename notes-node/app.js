f85ee89a34ffdc6a89adf4e13f906483e3c5ca8d
David Porter <davidbrianporter@gmail.com>
2017-01-27 08:50:18 -0500

    creating my own module

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,19 +5,21 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+//  ./ === current directory
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+// var user = os.userInfo();
 
-// causes a warning in ver7
-// fs.appendFile('greetings.txt', 'Hello World');
+// // causes a warning in ver7
+// // fs.appendFile('greetings.txt', 'Hello World');
 
-// future proof
-// use template strings
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
-    if (err) {
-        console.log('Unable to write to file');
-    }
-});
+// // future proof
+// // use template strings
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
+//     if (err) {
+//         console.log('Unable to write to file');
+//     }
+// });
 
 // future proof 2
 //fs.appendFileSync('greetings.txt', 'Hello world! opt 2');
